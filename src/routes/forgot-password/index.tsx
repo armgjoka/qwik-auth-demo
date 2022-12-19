9c4fe37c9a346c5ca7e173e073f274c54cad9e9f
armela <armela@builder.io>
2022-12-19 22:18:49 +0100

    add routes

---
+++ src/routes/forgot-password/index.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    return (
+      <div>
+        <h2>
+          Forgot Password Page
+        </h2>
+      </div>
+    );
+  });
---
+++ src/routes/register/index.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    return (
+      <div>
+        <h2>
+          Register Page
+        </h2>
+      </div>
+    );
+  });
